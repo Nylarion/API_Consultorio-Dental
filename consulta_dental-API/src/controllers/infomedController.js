b363533a0b05633bd672b5a0b8f5f79293fa6e90
Luis <134728490+Nylarion@users.noreply.github.com>
2025-08-30 18:15:57 -0400

    Update infomedController.js

--- consulta_dental-API/src/controllers/infomedController.js
+++ consulta_dental-API/src/controllers/infomedController.js
@@ -56,10 +56,58 @@ const infomedController = {
 
             const {id_InfoMedica} = req.params;
 
+            console.log(`Buscando Información Medica por ID: ${id_InfoMedica}`);
 
+            const InfoMed = await InfoMedica.findByPK(id_InfoMedica);
+            
+            if (!InfoMed){
+
+                console.log(`Información medica con ID ${id_InfoMedica} no encontrado`);
+                return res.status(404).json({
+
+                    mensaje: `Información medica con ID ${id_InfoMedica} no encontrado`
+
+                });
+
+            }
+
+            console.log(`Información medica encontrada: ${InfoMed.diagnostico}`)
+
+
+            res.status(200).json({
+
+                mensaje: 'Informacion medica encontrada exitosamente',
+                datos: InfoMed
+
+            });
+
+        } catch (error){
+
+            console.error('Error al objeter información medica: ', error.message);
+
+            res.status(500).json({
+
+                mensaje: 'Error interno del servidor',
+                error: error.message
+
+            });
 
         }
 
+
+    },
+
+
+    crear: async (req, res) => {
+
+        try {
+
+            const { diagnostico, tratamiento}
+
+
+
+
+        }
 
 
     }
